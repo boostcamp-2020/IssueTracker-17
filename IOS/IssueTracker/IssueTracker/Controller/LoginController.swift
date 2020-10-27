35856ea3f49efb880b5bde1691a6cfa5dc6b0f2b
Skydevilk <skydevilk@gmail.com>
2020-10-28 00:54:15 +0900

    feat : Apple Social Login 구현

--- IOS/IssueTracker/IssueTracker/Controller/LoginController.swift
+++ IOS/IssueTracker/IssueTracker/Controller/LoginController.swift
@@ -6,19 +6,39 @@
 //
 
 import UIKit
-
+import AuthenticationServices
 class LoginController: UIViewController {
-
-    @IBAction func githubLoginButtonTouch(_ sender: UIButton) {
+    
+    @IBAction func signInWithGithubButtonTouch(_ sender: UIButton) {
         LoginManager.shared.requestCodeToGithub()
     }
-    
-  
-
+    @IBAction func signInWithAppleButtonTouch(_ sender: UIButton) {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self as ASAuthorizationControllerDelegate
+        controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
+        controller.performRequests()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+}
+extension LoginController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let user = credential.user
+            print("User : \(user)")
+            if let email = credential.email {
+                print("Email : \(email)")
+            }
+            if let fullName = credential.fullName{
+                print("Full Name : \(fullName)")
+            }
+        }
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error)
+    }
 }
