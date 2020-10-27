b0a919719692c59ffd117c176856421cbbc77da3
JinGu <xortm854@naver.com>
2020-10-27 21:38:30 +0900

    feature : Back-End 라우트 및 컨트롤러 틀 생성
    일괄성있는 개발을 위해 기본적인 라우트, 컨트롤러 틀을 개발하였습니다.
    추가적인 구조가 변경될 수도 있습니다.

---
+++ Back-End/controller/label/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { label } = require('../../models/sequelize');
+
+function labelController() {}
+
+labelController.get = (req, res, next) => {};
+labelController.insert = (req, res, next) => {};
+labelController.update = (req, res, next) => {};
+labelController.delete = (req, res, next) => {};
+
+module.exports = labelController;
