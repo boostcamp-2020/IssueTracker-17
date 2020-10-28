ed2d8196a047cf585c1d1f6dc73ff90bea5e8a08
JinGu <xortm854@naver.com>
2020-10-28 21:29:41 +0900

    feature : Issue Read Controller
    Issue get 요청을 했을 때 사용자가 필요한 데이터를 모델로부터 받아오고
    보내주기 위해 Issue get Controller Method를 구현하였습니다

--- Back-End/controller/issue/index.js
+++ Back-End/controller/issue/index.js
@@ -1,12 +1,35 @@
 require('dotenv').config();
 const { issue } = require('../../models/sequelize');
+const model = require('../../models/sequelize');
 
 function issueController() {}
 
-issueController.get = (req, res, next) => {};
+issueController.get = async (req, res) => {
+    try {
+        const result = await issue.get({ model: model });
+        result.forEach((issueDataValue) => {
+            let data = issueDataValue['dataValues'];
+            data['userName'] = data['user']['name'];
+            data['milestoneTitle'] = data['milstone']
+                ? data['milstone']['title']
+                : data['milstone'];
+            data['user'] = undefined;
+            data['milestone'] = undefined;
+            data['labels'] = [];
+            data['has_labels'].forEach((label) => {
+                data['labels'] = [...data['labels'], label['label']];
+            });
+            data['has_labels'] = undefined;
+        });
+        res.status(200).json({ result: result });
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({ result: false });
+    }
+};
 issueController.detail = (req, res, next) => {};
-issueController.insert = (req, res, next) => {};
-issueController.update = (req, res, next) => {};
-issueController.delete = (req, res, next) => {};
+issueController.insert = async (req, res) => {};
+issueController.update = async (req, res) => {};
+issueController.delete = async (req, res) => {};
 
 module.exports = issueController;
