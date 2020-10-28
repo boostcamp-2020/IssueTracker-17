bc94aff44cacca6201221f20b44d5c8734be866c
JinGu <xortm854@naver.com>
2020-10-28 20:48:35 +0900

    refactor : REST API 에 맞는 HTTP Method로 변경
    모두 Post Method를 사용하였지만 REST API의 HTTP Method 를 지키기 위해
    get, post, put, delete Method를 사용하였습니다.

--- Back-End/routes/label/index.js
+++ Back-End/routes/label/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const labelController = require('../../controller/label');
 
-router.post('/get', labelController.get);
+router.get('/get', labelController.get);
 router.post('/insert', labelController.insert);
-router.post('/update', labelController.update);
-router.post('/delete', labelController.delete);
+router.put('/update', labelController.update);
+router.delete('/delete', labelController.delete);
 
 module.exports = router;
