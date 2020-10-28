5460b1b594bbc62f9b540b72161cdd29fef71873
Dongmin Shin <nsdm821@gmail.com>
2020-10-29 03:22:39 +0900

    feature : issue Update 구현 rest api 호출시 issue  table의 변경하고 싶은 colum만 request body에 넣어주면 해당 부분만 수정됨

--- Back-End/controller/issue/index.js
+++ Back-End/controller/issue/index.js
@@ -29,7 +29,29 @@ issueController.get = async (req, res) => {
 };
 issueController.detail = (req, res, next) => {};
 issueController.insert = async (req, res) => {};
-issueController.update = async (req, res) => {};
+issueController.update = async (req, res) => {
+    const bodyObj = req.body;
+    const { id } = bodyObj;
+    console.log(bodyObj);
+    let modifyData = {};
+    modifyData.title = !bodyObj.title ? undefined : bodyObj.title;
+    modifyData.contents = !bodyObj.contents ? undefined : bodyObj.contents;
+    modifyData.created = !bodyObj.created ? undefined : bodyObj.created;
+    modifyData.milestone_id =
+        bodyObj.milestoneId === undefined ? undefined : bodyObj.milestoneId;
+    modifyData.status = !bodyObj.status ? undefined : bodyObj.status;
+
+    modifyData = JSON.parse(JSON.stringify(modifyData));
+    //console.log(modifyData);
+    //console.log(id);
+    try {
+        await issue.update(modifyData, { where: { id } });
+        res.status(200).json({ result: true });
+    } catch (e) {
+        console.error(e);
+        res.status(400).json({ result: false });
+    }
+};
 issueController.delete = async (req, res) => {};
 
 module.exports = issueController;
