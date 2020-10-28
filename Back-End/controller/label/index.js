e2708fcac5d0d5843c13dbebd19e5dd8a2de68b2
JinGu <xortm854@naver.com>
2020-10-28 21:00:59 +0900

    feature : Label CRUD Controller
    Label의 CRUD를 위한 컨트롤러가 필요하기 때문에 route로부터 미들웨어
    역학을 하는 controller를 구현하여 요청값을 받아 모델로 부터 사용자가
    원하는 데이터를 받아 다시 보내주는 작업을 진행하였습니다

--- Back-End/controller/label/index.js
+++ Back-End/controller/label/index.js
@@ -3,9 +3,49 @@ const { label } = require('../../models/sequelize');
 
 function labelController() {}
 
-labelController.get = (req, res, next) => {};
-labelController.insert = (req, res, next) => {};
-labelController.update = (req, res, next) => {};
-labelController.delete = (req, res, next) => {};
+labelController.get = async (req, res) => {
+    try {
+        const result = await label.get();
+        res.status(200).json({ result: result });
+    } catch (e) {
+        res.status(400).json({ result: false });
+    }
+};
+labelController.insert = async (req, res) => {
+    const { title, contents, color } = req.body;
+    try {
+        const result = await label.insert({
+            title: title,
+            contents: contents,
+            color: color,
+        });
+        res.status(200).json({ result: true, id: result.id });
+    } catch (e) {
+        res.status(400).json({ result: false });
+    }
+};
+labelController.update = async (req, res) => {
+    const { id, title, contents, color } = req.body;
+    try {
+        const result = await label.change({
+            id: id,
+            title: title,
+            contents: contents,
+            color: color,
+        });
+        res.status(200).json({ result: true, id: result.id });
+    } catch (e) {
+        res.status(400).json({ result: false });
+    }
+};
+labelController.delete = async (req, res) => {
+    const { id } = req.body;
+    try {
+        const result = await label.delete({ id: id });
+        res.status(200).json({ result: true, id: result.id });
+    } catch (e) {
+        res.status(400).json({ result: false });
+    }
+};
 
 module.exports = labelController;
