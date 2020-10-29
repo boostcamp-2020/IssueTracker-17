cb2d811378d7cbf068e56ed34f0ca7e6544111df
JinGu <xortm854@naver.com>
2020-10-29 11:33:44 +0900

    feature : has_label Model CRUD 구현 및 라우터추가
    has_label 모델에 관해 CRUD 작업을 진행하기위해 bulk Insert를 진행하고,
    update 및 list delete 관련 함수를 구현하였습니다.

--- Back-End/app.js
+++ Back-End/app.js
@@ -12,6 +12,7 @@ const labelRouter = require('./routes/label');
 const milestoneRouter = require('./routes/milestone');
 const issueRouter = require('./routes/issue');
 const commentRouter = require('./routes/comment');
+const hasLabelRouter = require('./routes/has_label');
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use('/label', labelRouter);
 app.use('/milestone', milestoneRouter);
 app.use('/issue', issueRouter);
 app.use('/comment', commentRouter);
+app.use('/has-label', hasLabelRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
