cbe2661bfd0b3b840232caa4be66efa525298f48
JinGu <xortm854@naver.com>
2020-10-30 11:20:14 +0900

    fix : 오타수정
    url 오타수정

--- Back-End/routes/has_assignee/index.js
+++ Back-End/routes/has_assignee/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const assigneeController = require('../../controller/assignee');
 
-router.get('/get:/issueId', assigneeController.get);
+router.get('/get/:issueId', assigneeController.get);
 router.post('/insert', assigneeController.insert);
 router.put('/update', assigneeController.update);
 router.delete('/delete', assigneeController.delete);
