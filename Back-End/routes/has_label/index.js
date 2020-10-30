5bc62350efcc2861c42865d9e5b4c032aa2fa297
JinGu <xortm854@naver.com>
2020-10-30 10:51:13 +0900

    fix : get Method에 맞는 REST API 적용
    이름은 get이었지만 post Method를 사용하고 있기 때문에 get이라는 이름에
    맞게 get/:변수이름 형태로 url을 바꿔서 입력받도록 하였습니다.

--- Back-End/routes/has_label/index.js
+++ Back-End/routes/has_label/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const hasLabelController = require('../../controller/has_label');
 
-router.post('/get', hasLabelController.get);
+router.get('/get/:issueId', hasLabelController.get);
 router.post('/insert', hasLabelController.insert);
 router.put('/update', hasLabelController.update);
 router.delete('/delete', hasLabelController.delete);
