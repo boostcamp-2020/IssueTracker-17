9699beb87d17faf6c0349f91ffc6b54be6d969f1
sw15_sunkyu <azon731@gmail.com>
2020-11-02 18:58:15 +0900

    feature : front eslint 설정
    eslint 적용
    rules: (eslint:recommended, react/recommended, google, prettier)
    eslint jsx 사용 설정
    eslint-google rules 중 require-jsdoc rule 해제
    prettier 연동

--- Front-End/webpack.config.js
+++ Front-End/webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: './index.js',
-    output: {
-        publicPath : '/',
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_module/,
-                use:{
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
