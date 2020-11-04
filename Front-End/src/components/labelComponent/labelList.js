7cc6091cefe3abdfd47b2b5732273429c76c888e
JinGu <xortm854@naver.com>
2020-11-04 18:20:48 +0900

    fix : Label 컴포넌트에 key값 추가
    반복되는 컴포넌트를 구현할 때는 구별할 수 있는 key 값이 필요하기 때문에
    Label data의 id 값으로 Key값을 지정해줬습니다.

--- Front-End/src/components/labelComponent/labelList.js
+++ Front-End/src/components/labelComponent/labelList.js
@@ -5,7 +5,7 @@ import { Label } from './makeLabel';
 export function LabelList() {
   const { labelList } = useContext(PostsContext);
 
-  const list = labelList.map((label,idx) => <Label data={label} className={'main'} idx={idx} />);
+  const list = labelList.map((label,idx) => <Label key={label.id} data={label} className={'main'} idx={idx} />);
 
   return <div className="labelList-form">{list}</div>;
 }
