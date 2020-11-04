37478214a584c282e3c28ecc477cc7881e897648
JinGu <xortm854@naver.com>
2020-11-05 01:53:26 +0900

    fix : 클래스를 함수형으로 변환
    초기 코드로 클래스로 선언되어있어 놓치고 넘어간 부분을 수정합니다.

--- Front-End/src/app.js
+++ Front-End/src/app.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import * as Pages from "./pages";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import * as Pages from './pages';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exac path="/" component={Pages.LabelPage} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exac path="/" component={Pages.LabelPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
