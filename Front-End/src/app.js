f5c3cdaa2a3da30639a3925aa653d4da158cf714
sunkyu_yoo <40164248+sunkest@users.noreply.github.com>
2020-11-05 18:26:22 +0900

    Update app.js

--- Front-End/src/app.js
+++ Front-End/src/app.js
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route exac path="/" component={Pages.LabelPage} />
-        <Route exac path="/newIssue" component={Pages.NewIssuePage} /> */}
+        <Route exac path="/" component={Pages.LabelPage} />
+        <Route exac path="/newIssue" component={Pages.NewIssuePage} />
         <Route exac path="/" component={Pages.LoginPage} />
       </Switch>
     </BrowserRouter>
