f6e7ead9464d148d1604a3bfd9b4d172317d7c76
Dongmin Shin <nsdm821@gmail.com>
2020-11-05 11:59:51 +0900

    feature: issue 조건 필터링을 위한 query 추가 filterQuery를 where절에 추가, label을 제외한 모든 조건 필터링 적용

--- Back-End/models/issue.js
+++ Back-End/models/issue.js
@@ -1,3 +1,5 @@
+milestoneconst { query } = require('express');
+
 module.exports = (sequelize, Datatypes) => {
     const issue = sequelize.define(
         'issue',
@@ -60,8 +62,8 @@ module.exports = (sequelize, Datatypes) => {
         });
     };
 
-    issue.get = async ({ model }) => {
-        const result = await issue.findAll({
+    issue.get = async ({ model, id, filterQuery }) => {
+        const query = {
             include: [
                 {
                     model: model.user,
@@ -69,7 +71,7 @@ module.exports = (sequelize, Datatypes) => {
                 },
                 {
                     model: model.milestone,
-                    attributes: ['title'],
+                    // attributes: ['title'],
                 },
                 {
                     model: model.has_label,
@@ -93,9 +95,16 @@ module.exports = (sequelize, Datatypes) => {
                         },
                     ],
                 },
+                {
+                    model: model.comment,
+                    attributes: ['user_id'],
+                },
             ],
+            where: filterQuery,
             attributes: ['id', 'title', 'status', 'contents', 'created'],
-        });
+        };
+        if (id) query.where = { id };
+        const result = await issue.findAll(query);
         return result;
     };
 
