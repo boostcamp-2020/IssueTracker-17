a074fb1633947e866f08be91c78f5955f030bdb3
Dongmin Shin <nsdm821@gmail.com>
2020-11-10 08:37:35 +0900

    feature: MileStoneRpw Component 구현 milestone 목록에서 row에 해당하는 컴포넌트

---
+++ Front-End/src/components/MileStoneListComponent/mileStoneRow.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import React from 'react';
+import styled from 'styled-components';
+import { ProgressBar } from './ProgressBar';
+
+const RowContainer = styled.div`
+  padding: 5px;
+  border-bottom: 1px solid rgb(225 228 232);
+  display: flex;
+  justify-content: stretch;
+`;
+
+const MileStoneContainer = styled.div`
+  width: 50%;
+`;
+
+const ProgressContainer = styled.div`
+  width: 50%;
+`;
+
+export const MileStoneRow = (props) => {
+  const { title, contents, until } = props.data;
+  const opened = 10;
+  const closed = 7;
+  return (
+    <RowContainer>
+      <MileStoneContainer>
+        <h2>{title}</h2>
+        <div>{contents}</div>
+        <div>{until}</div>
+      </MileStoneContainer>
+      <ProgressContainer>
+        <ProgressBar width="70%"></ProgressBar>
+        <div>
+          70% complete {opened} opened {closed} closed
+        </div>
+        <button>edit</button> <button>close</button> <button>delete</button>
+      </ProgressContainer>
+    </RowContainer>
+  );
+};
