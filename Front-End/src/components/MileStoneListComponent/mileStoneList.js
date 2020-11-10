7f0e180704c02c9f4500a9c6457d5138402bb511
Dongmin Shin <nsdm821@gmail.com>
2020-11-10 11:27:58 +0900

    refactor: milestone sample data삭제, backend연동코드 이동 backend 연동해서 milestone list가져오는 코드를 MileStoneListComponent로 이동 테스트용으로 만든 milestone sample data 생성 코드를 삭제

--- Front-End/src/components/MileStoneListComponent/mileStoneList.js
+++ Front-End/src/components/MileStoneListComponent/mileStoneList.js
@@ -2,38 +2,21 @@
 import React from 'react';
 import { MileStoneRow } from './mileStoneRow';
 import styled from 'styled-components';
-import axios from 'axios';
 
-const getMileStoneList = () => {
-  const apiUrl = 'http://localhost:3000/milestone/';
-  axios
-    .get(apiUrl)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((err) => {
-      alert(err);
-    });
-};
-
-export const MileStoneList = () => {
+export const MileStoneList = (props) => {
+  /*
   const data = {
     title: '스프린트',
     contents: '내용',
     until: new Date().toLocaleString(),
   };
-
-  const datalist = [];
-  for (let i = 0; i < 10; i++) {
-    const newdata = Object.assign({}, data);
-    newdata.title += ' ' + i;
-    newdata.contents += ' ' + i;
-    datalist.push(newdata);
-  }
+*/
+  const datalist = props.mileStoneList;
+  // console.log(datalist);
 
   const milestonelist = datalist.map((data, idx) => (
     <MileStoneRow key={idx} data={data}></MileStoneRow>
   ));
-  getMileStoneList();
+
   return milestonelist;
 };
