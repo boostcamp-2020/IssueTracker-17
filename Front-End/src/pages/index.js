5133606b2ef9a9e99aa0da4f10d158cc0bcdacc1
sdm821 <nsdm821@gmail.com>
2020-11-10 19:13:59 +0900

    feature: IssueListPage 추가

--- Front-End/src/pages/index.js
+++ Front-End/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as LabelPage } from './labelPage';
 export { default as NewIssuePage } from './NewIssuePage';
 export { default as LoginPage } from './LoginPage';
 export { default as MileStoneListPage } from './MileStoneListPage';
+export { default as IssueListPage } from './IssueListPage';
