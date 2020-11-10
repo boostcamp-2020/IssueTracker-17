a577306655251b6dcd28365d94ea22eb5fea82d8
JinGu <xortm854@naver.com>
2020-11-11 01:55:08 +0900

    refactor : Display 상태변수 생성 및 사용
    콤포넌트의 display를 결정하기 위해서 document API 를 사용하지 않고
    styled-component와 함께 상태를 관리하여 display값을 정해줬습니다.

--- Front-End/src/components/labelComponent/LabelList/LabelList.js
+++ Front-End/src/components/labelComponent/LabelList/LabelList.js
@@ -1,11 +1,24 @@
 import React, { useContext } from 'react';
-import { PostsContext } from './LabelComponent';
-import { Label } from './makeLabel';
+import { PostsContext } from '../LabelComponent';
+import { Label } from './LabelBox/LabelBox';
+import { LabelNavBar } from './LabelNavBar/LabelNavBar';
+import styled from 'styled-components';
+
+const LabelListForm = styled.div`
+  display: block;
+`;
 
 export function LabelList() {
   const { labelList } = useContext(PostsContext);
 
-  const list = labelList.map((label,idx) => <Label key={label.id} data={label} className={'main'} idx={idx} />);
+  const list = labelList.map((label, idx) => (
+    <Label key={label.id} data={label} className={'main'} idx={idx} />
+  ));
 
-  return <div className="labelList-form">{list}</div>;
+  return (
+    <LabelListForm>
+      <LabelNavBar />
+      {list}
+    </LabelListForm>
+  );
 }
