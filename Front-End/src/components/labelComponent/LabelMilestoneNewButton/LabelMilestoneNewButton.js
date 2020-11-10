046c3e5aa17e3e7df69a490a3bc930d5f8a834c1
JinGu <xortm854@naver.com>
2020-11-11 01:58:17 +0900

    refactor : 재사용성 컴포넌트들을 사용하여 새로운 컴포넌트 구현
    해당 컴포넌트는 다른 페이지에서 재사용되게 때문에 이것을 이용하기 위해
    재사용가능한 컴포넌트들로 새로운 컴포넌트를 구성하였습니다.

--- Front-End/src/components/labelComponent/LabelMilestoneNewButton/LabelMilestoneNewButton.js
+++ Front-End/src/components/labelComponent/LabelMilestoneNewButton/LabelMilestoneNewButton.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import { Label } from '../makeLabel';
+import React, { useState } from 'react';
+import { Label } from '../LabelList/LabelBox/LabelBox';
 import { LabelButton, MilestoneButton } from 'Components/common';
 import { GreenButton } from 'Style';
+import styled from 'styled-components';
 
-export function TopLinks() {
-  function toggleLabelInputEventHandler(e) {
-    const detailForm = document.querySelector('.top-input-wrapper');
-    detailForm.classList.toggle('hidden');
+const Wrapper = styled.div`
+  width: 16%;
+  margin-right: auto;
+`;
+
+const ButtonsWrapper = styled.div`
+  margin: 20px 0px;
+  display: flex;
+`;
+
+const ButtonsForm = styled.div`
+  margin-bottom: 20px;
+`;
+
+export function LabelMilestoneNewButton() {
+  const [labelVisible, setLabelVisible] = useState(false);
+  function toggleLabelInputEventHandler() {
+    setLabelVisible(!labelVisible);
   }
 
   return (
-    <div className="top-links-form">
-      <div className="top-links">
-        <div className="top-links-btn-wrapper">
+    <ButtonsForm>
+      <ButtonsWrapper>
+        <Wrapper>
           <LabelButton />
           <MilestoneButton />
-        </div>
-        <GreenButton onClick={(e) => toggleLabelInputEventHandler(e)}>
+        </Wrapper>
+        <GreenButton onClick={(e) => toggleLabelInputEventHandler()}>
           New Label
         </GreenButton>
-      </div>
-      <Label className={'top-input'} flag={1} />
-    </div>
+      </ButtonsWrapper>
+      <Label className={'top-input'} flag={1} labelVisible={labelVisible} toggleHandler={setLabelVisible} />
+    </ButtonsForm>
   );
 }
