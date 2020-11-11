7046570f9e9cb0260c419c271829e8ff884e7c70
Dongmin Shin <nsdm821@gmail.com>
2020-11-12 04:48:01 +0900

    feature : EditMileStonePage 추가

--- Front-End/src/pages/index.js
+++ Front-End/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as LoginPage } from './LoginPage';
 export { default as Auth } from './Auth';
 export { default as MileStoneListPage } from './MileStoneListPage';
 export { default as IssueListPage } from './IssueListPage';
+export { default as EditMileStonePage } from './EditMileStonePage';
