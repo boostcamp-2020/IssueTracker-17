49edb09e4c769fd2f14f42170fc48421fb1ff16a
Dongmin Shin <nsdm821@gmail.com>
2020-11-12 13:16:35 +0900

    feature : filter bar css 조정

--- Front-End/src/components/IssueList/FilterBar.js
+++ Front-End/src/components/IssueList/FilterBar.js
@@ -5,20 +5,28 @@ import styled from 'styled-components';
 const FilterBar = styled.div`
   //border: 1px solid rgb(225 228 232);
   height: 40px;
-  width: 100%;
-  padding: 5px;
   display: flex;
-  background-color: #f4f4f4;
+  border-radius: 2px;
 `;
 const FilterSelector = styled.select`
   border: 1px solid rgb(225 228 232);
-  width: 50px;
+  width: 100px;
   background-color: #f4f4f4;
 `;
-const SearchInputBox = styled.div`
-  border: 1px solid rgb(225 228 232);
-  background-color: #f4f4f4;
-  width: 100% !important;
+
+const InputForm = styled.input`
+  padding: 5px 12px;
+  margin: 2px;
+  font-size: 14px;
+  line-height: 20px;
+  width: 450px;
+  vertical-align: middle;
+  background-repeat: no-repeat;
+  background-position: right 8px center;
+  border: 1px solid #e1e4e8;
+  border-radius: 6px;
+  outline: none;
+  box-shadow: inset 0 1px 0 rgba(225, 228, 232, 0.2);
 `;
 
 export const FilterBarComponent = (props) => {
@@ -28,7 +36,7 @@ export const FilterBarComponent = (props) => {
         <option value="1">Filters</option>
       </FilterSelector>
 
-      <SearchInputBox>search all issues</SearchInputBox>
+      <InputForm placeholder="search all issues"></InputForm>
     </FilterBar>
   );
 };
